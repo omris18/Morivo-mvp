927b60fa508d8daca9c852ec7ed1209e245e3ffc
omris18 <omris18@gmail.com>
2026-08-12 01:19:40 +0700

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,5 @@
-
+export const metadata = { title: "Morivo MVP", description: "Experience Operating System" };
+import "./globals.css";
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
